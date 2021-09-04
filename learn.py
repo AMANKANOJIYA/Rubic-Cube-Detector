e2abd1fa7076bf3488adc266728074574f4e6332
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-09-04 09:42:21 +0530

    New Version

--- learn.py
+++ learn.py
@@ -9,13 +9,13 @@ gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # # Calculation of Sobely
 # sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
 laplasion=cv2.Laplacian(img,cv2.CV_64F)
-edged = cv2.Canny(laplasion, 20, 200)
+# edged = cv2.Canny(laplasion, 20, 200)
 
 # cv2.imshow("Image1",sobelx)
 # cv2.imshow("Image2",sobely)
 cv2.imshow("Image3",laplasion)
 cv2.imshow("Image4",img)
-cv2.imshow("Image5",edged)
+# cv2.imshow("Image5",edged)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
